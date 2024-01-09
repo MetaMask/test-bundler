d138a4309605417ce9c3958da78c8a9aaa801526
Matthew Walsh <matthew.walsh@consensys.net>
2024-01-09 08:08:12 +0000

    Convert monorepo to single package
    Remove excess files.

---
+++ src/contracts/Import.sol
@@ -0,0 +1,6 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// import contracts to get their type info.
+import "./test/SampleRecipient.sol";
+import "@account-abstraction/contracts/samples/SimpleAccountFactory.sol";
